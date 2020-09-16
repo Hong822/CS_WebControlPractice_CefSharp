59182a4f5d9aa23d57d3e809fe7526d3c012a035
Hong822 <47422913+Hong822@users.noreply.github.com>
2020-09-16 19:39:51 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CS_WebControlPractice_CefSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChromeBrowser();
+        }
+
+        private void InitializeChromeBrowser()
+        {
+            CefSettings cefSettings = new CefSettings();
+            Cef.Initialize(cefSettings);
+            ChromiumWebBrowser chrome = new ChromiumWebBrowser("https://www.naver.com");
+            chrome.Dock = DockStyle.Fill; //Maximize size
+            this.Controls.Add(chrome);
+        }
+    }
+}
